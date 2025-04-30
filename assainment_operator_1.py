39819180781f22dfa52feda11d9a5f2d95d949bf
AdithyaKumar <kumaradithya537@gmail.com>
2025-04-30 13:09:08 +0530

    assainment operators

---
+++ assainment_operator_1.py
@@ -0,0 +1,8 @@
+a=10
+print(a)
+print(type(a))
+print(id(a))
+a=22
+print(a)
+print(type(a))
+print(id(a))
---
+++ assainment_operator_1_1.py
@@ -0,0 +1,8 @@
+a=10.0
+print(a)
+print(type(a))
+print(id(a))
+a+=22
+print(a)
+print(type(a))
+print(id(a))
